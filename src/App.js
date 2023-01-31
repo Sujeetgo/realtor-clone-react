43a1b67920c532ef26c174b04fac5b71302fb668
sujeet kumar <sujeetkr2508@gmail.com>
2023-01-31 10:52:03 +0530

    create pagesa and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ForgotPassword from "./pages/ForgotPassword";
+import Home from "./pages/Home";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element = {<Home/>}/>
+          <Route path="/profile" element = {<Profile/>}/>
+          <Route path="/signin" element = {<SignIn/>}/>
+          <Route path="/signup" element = {<SignUp/>}/>
+          <Route path="/forgotpassword" element = {<ForgotPassword/>}/>
+          <Route path="/offers" element = {<Offers/>}/>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/pages/Offers.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Offers = () => {
+  return (
+    <div>
+      Offers page 
+    </div>
+  )
+}
+
+export default Offers
